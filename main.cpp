81446a725ad5922c4d890b22f30f7f09fe6ac1fe
Vintter <88837601+Vintter@users.noreply.github.com>
2021-08-18 23:19:48 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,19 +11,12 @@ using namespace std;
 
 class No{
 public:
-    uint32_t move;
-    bool x;//verifica se ja houve uma passagem por esse no
+    uint32_t height,prev;
+    vector<uint32_t> next;
 };
 
-bool contains(uint32_t n,vector<No> v){
-    for(auto i:v)
-        if (n == i.move)
-            return true;
-    return false;
-}
-
 bool containsVector(uint32_t n,vector<uint32_t> v){
-    for(auto i:v)
+    for(uint32_t i:v)
         if (n == i)
             return true;
     return false;
@@ -31,7 +24,8 @@ bool containsVector(uint32_t n,vector<uint32_t> v){
 
 int main() {
 
-    uint32_t N,S,bosses=0,start,x;
+    uint32_t N,S,bosses=0,x;
+    vector<uint32_t> root;
 
 #ifdef LOCAL
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
@@ -39,34 +33,64 @@ int main() {
 
     cin>>N>>S;
     vector<No> node (N+1);
+    root.push_back(1);
     for(uint32_t i=2;i<=N;i++){
         cin>>x;
-      node[i].move = x;
+        node[i].prev = x;
     }
 
-    while(S>0) {
+    for(uint32_t i=2; i<=N;i++){
         vector<uint32_t> s;
         uint32_t h=0;
-        for(uint32_t i=2;i<=N;i++) {
-            x=0;
-            uint32_t y=i;
-            while(y != 0 && !node[y].x && !containsVector(y,s)){
-                s.push_back(y);
-                x++;
-                y = node[y].move;
-            }
-            s.clear();
-            if(x>h){
-                h = x;
-                start = i;
+        uint32_t j=i,a;
+        bool y = false;
+        while(!y){
+            h++;
+            if(node[j].height < h){
+                if(!containsVector(j,s)){
+                    node[j].height = h;
+                    if(j != 1 && !containsVector(node[j].prev,s) && !containsVector(j,node[node[j].prev].next))
+                        node[node[j].prev].next.push_back(j);
+                    else if(j == 1)
+                        y = true;
+                }else{
+                    if(!containsVector(a,root))
+                        root.push_back(a);
+                    y = true;
+                }
+                s.push_back(j);
+                a=j;
+                j=node[j].prev;
+            }else{
+                y = true;
             }
         }
-        h=0;
-        while(start != 0 && !node[start].x && !containsVector(start,s)){
+        s.clear();
+    }
+
+    while(S>0){
+        x = 0;
+        for(uint32_t i : root){
+            if(node[i].height > node[x].height){
+                x = i;
+            }
+        }
+        uint32_t l=node[x].height;
+        while(l>0){
+            uint32_t m=0;
             bosses++;
-            node[start].x = true;
-            s.push_back(start);
-            start = node[start].move;
+            for(uint32_t i : node[x].next){
+                if(node[i].height > node[m].height){
+                    m = i;
+                }
+                root.push_back(i);
+            }
+            if(containsVector(x,root)){
+                erase(root,x);
+            }
+            node[x].height = 0;
+            x = m;
+            l--;
         }
         S--;
     }
